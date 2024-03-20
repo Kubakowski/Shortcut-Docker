4a58406dd22f09b61218530c610261337c28b46b
Jakub Wienczkowski <kubakowski14@gmail.com>
2024-03-20 17:13:45 -0400

    Fixed Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate, Link } from 'react-router-dom'; // Import Link and useNavigate
+import { useNavigate } from 'react-router-dom';
 import profileIcon from "../assets/profile-icon.png";
 import pinIcon from "../assets/pin-icon.png";
 import settingsIcon from "../assets/settings-icon.png";
@@ -11,22 +11,21 @@ function Navbar() {
     <div className='navbar-wrapper'>
       {/* Profile Image as Link */}
       <div className='profile-wrapper'>
-        <Link to="/profile">
+        <button onClick={() => navigate("/profile")}>
           <img src={profileIcon} alt="Profile" />
-        </Link>
+        </button>
       </div>
 
       {/* Navigation Options */}
       <div className='tabs-wrapper'>
         <div className='nav-btn-wrapper' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px', marginTop: '10px' }}>
-          {/* Using Link components for navigation */}
-          <Link to="/shortcuts">Shortcuts</Link>
-          <Link to="/dock">Dock</Link>
-          <Link to="/settings">Settings</Link>
-
-          {/* Alternative buttons to navigate, demonstrating useNavigate hook */}
+          {/* Buttons to navigate, demonstrating useNavigate hook */}
           <button onClick={() => navigate("/shortcuts")}>
             <img src={pinIcon} alt="Shortcuts" />
+          </button>
+          <button onClick={() => navigate("/dock")}>
+            {/* Assuming you have an icon for 'Dock', you can add it here */}
+            {/* <img src={dockIcon} alt="Dock" /> */}
           </button>
           <button onClick={() => navigate("/settings")}>
             <img src={settingsIcon} alt="Settings" />
