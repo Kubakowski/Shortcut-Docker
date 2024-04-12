4c5b0587979782e00010d883a44930c9ca8b4a1e
Jakub Wienczkowski <kubakowski14@gmail.com>
2024-04-11 20:03:55 -0400

    Icons are now displayed on shortcuts

--- src/pages/Shortcuts.tsx
+++ src/pages/Shortcuts.tsx
@@ -1,4 +1,5 @@
 // Shortcuts.tsx
+// Need to make a change so new branch
 import React, { useState, useEffect } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../firebaseInit';
